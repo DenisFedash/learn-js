804b6a76f99134051644d586c8e6d71dacf1409a
DenisFedash <0501054104d@gmail.com>
2023-04-19 22:44:20 +0300

    add tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return <h1>Test 19.04.2023</h1>;
+
+  function nbYear(p0, percent, aug, p) {
+    let people = p0;
+    const percentage = percent / 100;
+    let years = 0;
+    for (let index = 1; people < p; index++) {
+      people = Math.floor(people + people * percentage + aug);
+      years = index;
+    }
+
+    return years;
+  }
+
+  function nbYear(p0, percent, aug, p) {
+    for (var years = 0; p0 < p; years++) {
+      p0 = Math.floor(p0 + (p0 * percent) / 100 + aug);
+    }
+    return years;
+  }
+  // console.log(nbYear(1500, 5, 100, 5000));
+  // console.log(nbYear(1500000, 2.5, 10000, 2000000));
+  // console.log(nbYear(1500000, 0.25, 1000, 2000000));
+
+  function solution(string) {
+    return string
+
+      .split(/([A-Z][a-z]+)/)
+      .filter((str) => Boolean(str))
+      .join(" ");
+  }
+  // console.log(solution("camelCasing"));
+  // console.log(solution("camelCasingTest"));
+
+  function createPhoneNumber(numbers) {
+    let format = "(xxx) xxx-xxxx";
+
+    for (let i = 0; i < numbers.length; i++) {
+      format = format.replace("x", numbers[i]);
+    }
+
+    return format;
+  }
+
+  // function createPhoneNumber(arr) {
+  //   return `(${arr.slice(0, 3).join("")}) ${arr.slice(3, 6).join("")}-${arr
+  //     .slice(6)
+  //     .join("")}`;
+  // }
+
+  // function createPhoneNumber(numbers) {
+  //   return numbers.join("").replace(/(...)(...)(.*)/, "($1) $2-$3");
+  // }
+  console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 }
 
 export default App;
